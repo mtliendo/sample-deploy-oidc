b1c8284baf132b9a81f7b48f6803007be635224e
focusotter <mtliendo@focusotter.com>
2023-07-04 22:11:33 -0500

    update with correct queue name

--- lib/app/app-stack.ts
+++ lib/app/app-stack.ts
@@ -1,18 +1,16 @@
 import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as sqs from 'aws-cdk-lib/aws-sqs'
+import { getCDKContext } from '../../utils'
 
-type AppStackProps = cdk.StackProps & {
-	stage?: string
-}
 export class AppStack extends cdk.Stack {
-	constructor(scope: Construct, id: string, props: AppStackProps) {
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props)
 
 		// The code that defines your stack goes here
-
+		const context = getCDKContext(this)
 		// example resource
-		const queue = new sqs.Queue(this, `AppQueue-${props.stage}`, {
+		const queue = new sqs.Queue(this, `AppQueue-${context.stage}`, {
 			visibilityTimeout: cdk.Duration.seconds(300),
 		})
 	}
